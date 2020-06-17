89bfe5bf61bdeab49279fc24f840b15a32a411a4
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-06-17 19:30:24 +0530

    dialogs added

---
+++ photo_diary/lib/components/newFriendAdd.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:photo_diary/utils/hexColor.dart';
+
+class NewFriendDialog extends StatelessWidget {
+  TextEditingController _textEditingController = TextEditingController();
+
+  dialogContent(BuildContext context) {
+    return Container(
+      height: 300.0,
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 20.0),
+            child: Text(
+              'Add a new Friend!',
+              style: GoogleFonts.pacifico(
+                textStyle: TextStyle(
+                  letterSpacing: 2.0,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              maxLines: 1,
+              style: GoogleFonts.pacifico(
+                textStyle: TextStyle(
+                  letterSpacing: 2.0,
+                ),
+              ),
+              controller: _textEditingController,
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {},
+            color: HexColor('#8FB9A8'),
+            child: Text(
+              'Add!',
+              style: GoogleFonts.pacifico(
+                textStyle: TextStyle(
+                  letterSpacing: 2.0,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      elevation: 2.0,
+      backgroundColor: Colors.white,
+      child: dialogContent(context),
+    );
+  }
+}
--- photo_diary/lib/components/viewPersons.dart
+++ photo_diary/lib/components/viewPersons.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:photo_diary/components/newFriendAdd.dart';
 import 'package:photo_diary/utils/consts.dart';
+import 'package:photo_diary/utils/hexColor.dart';
 
 class FriendsDialog extends StatelessWidget {
   final List dataVal;
@@ -32,48 +34,78 @@ class FriendsDialog extends StatelessWidget {
               ),
             ],
           ),
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  print(dataVal[index]);
-                  Navigator.pop(context, index);
-                },
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(
-                                  12.0, 12.0, 12.0, 6.0),
-                              child: Text(
-                                dataVal[index],
-                                style: GoogleFonts.pacifico(
-                                    textStyle: TextStyle(
-                                  letterSpacing: 1.0,
-                                  fontSize: 15.0,
-                                )),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Divider(
-                      height: 10.0,
-                      color: Colors.grey,
-                    ),
-                  ],
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'People You\'ve added',
+                style: GoogleFonts.pacifico(
+                  textStyle: TextStyle(letterSpacing: 2.0, fontSize: 20.0),
                 ),
-              );
-            },
-            itemCount: dataVal.length,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        print(dataVal[index]);
+                        Navigator.pop(context, index);
+                      },
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        12.0, 12.0, 12.0, 6.0),
+                                    child: Text(
+                                      dataVal[index],
+                                      style: GoogleFonts.pacifico(
+                                          textStyle: TextStyle(
+                                        letterSpacing: 1.0,
+                                        fontSize: 15.0,
+                                      )),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Divider(
+                            height: 10.0,
+                            color: Colors.grey,
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: dataVal.length,
+                ),
+              ),
+              FloatingActionButton(
+                elevation: 4.0,
+                onPressed: () async {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) => NewFriendDialog(),
+                  );
+                },
+                backgroundColor: HexColor('#8FB9A8'),
+                child: Icon(
+                  Icons.person_add,
+                ),
+              )
+            ],
           ),
         ),
       ],
